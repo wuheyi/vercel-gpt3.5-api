04c020b2560a958f0343c9429692c123abe32f66
fmw666 <fmw19990718@qq.com>
2023-03-10 17:30:37 +0800

    initial

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import openai
+from fastapi import FastAPI
+
+from models import ChatGPTReq
+
+
+app = FastAPI()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello World"}
+
+@app.post("/chatgpt")
+async def chatgpt(req: ChatGPTReq):
+    """
+    请求格式：
+    {
+        "messages": [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "I need help."},
+            {"role": "assistant", "content": "How can I help you?"},
+            ...
+        ]
+    }
+    """
+    return openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=req.messages
+    )
